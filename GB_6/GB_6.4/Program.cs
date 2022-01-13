2a0f5ca5b76b2f1147b012dea02ff378eb184bbb
SivachenkoEgor <georgegraygithub@gmail.com>
2022-01-14 04:48:31 +0700

    Have done two tasks in 6th homework

---
+++ GB_6/GB_6.4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GB_6._4
+{
+ /*   4. ** Считайте файл различными способами.Смотрите “Пример записи файла различными способами”.
+        Создайте методы, которые возвращают массив byte (FileStream, BufferedStream), строку для StreamReader и массив int для BinaryReader.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
