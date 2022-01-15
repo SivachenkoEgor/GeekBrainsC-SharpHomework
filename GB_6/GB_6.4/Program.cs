f3e2775133f8f15cd7ed44e5e36569c04eeb9b5e
SivachenkoEgor <georgegraygithub@gmail.com>
2022-01-16 02:31:50 +0700

    solve 4th task

--- GB_6/GB_6.4/Program.cs
+++ GB_6/GB_6.4/Program.cs
@@ -2,13 +2,33 @@
 
 namespace GB_6._4
 {
- /*   4. ** Считайте файл различными способами.Смотрите “Пример записи файла различными способами”.
-        Создайте методы, которые возвращают массив byte (FileStream, BufferedStream), строку для StreamReader и массив int для BinaryReader.*/
+ /*  Сиваченко Егор Александрович
+  *  4. ** Считайте файл различными способами.Смотрите “Пример записи файла различными способами”.
+        Создайте методы, которые возвращают массив byte (FileStream, BufferedStream), 
+ строку для StreamReader 
+ и массив int для BinaryReader.*/
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (var num in FileOperations.FileStreamSample("test_file", 16))
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine(new string('-', 50));
+            foreach (var num in FileOperations.BinaryStreamSample("test_file", 16))
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine(new string('-', 50));
+
+            Console.WriteLine(FileOperations.StreamWriterSample("test_file", 16));
+
+            Console.WriteLine(new string('-', 50));
+            foreach (var num in FileOperations.BufferedStreamSample("test_file", 16,4))
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
